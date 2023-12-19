2608a21d22db54ae1af4ed302034fed1f92fef05
Banka24 <Aq567489012@gmail.com>
2023-12-19 12:12:53 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double s = 0, x, a;
+
+            Console.Write("Введите х: ");
+            x = double.Parse(Console.ReadLine());
+            a = x;
+            for(int i = 1; i <= 7; i++)
+            {
+                s = i % 2 != 0 ? s + a : s - a;
+                a *= x * x / (2 * i * (2 * i + 1));
+            }
+            Console.WriteLine($"S = {s:f3}");
+        }
+    }
+}
